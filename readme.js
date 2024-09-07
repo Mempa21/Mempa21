94113877fcdff49d1fd2cc48d8d202ddd467eb75
Mempa21 <mempa1peu@gmail.com>
2024-09-07 22:22:42 +0200

    Initial commit

---
+++ readme.js
@@ -0,0 +1,47 @@
+module.exports = `
+### Hi there 👋
+
+I build web applications and share my journey on [Twitter](https://x.com/Mempa21) and [YouTube](https://www.youtube.com/@Mempa21) !
+
+I also work on small projects on this GitHub to have some fun.
+
+### Contacte-moi
+
+- [Twitter](https://x.com/Mempa21)
+- [Youtube](https://www.youtube.com/@Mempa21)
+- [Mail](mailto:mempa1peu@gmail.com)
+
+### My Projects
+
+- [Mempa](https://mlv.sh/mempa)
+
+  My personal portfolio showcasing my projects, blog, and resume.
+
+- [Profame](https://mlv.sh/profame)
+
+  An automated tool that generates social media posts, helping developers manage their online presence effortlessly.
+
+- [Memlabs](https://mlv.sh/memlabs)
+
+  A centralized platform that aggregates and summarizes key tech news, providing developers with a curated newsletter and blog to keep them informed without being overwhelmed.
+
+- [DocuGen](https://mlv.sh/docugen)
+
+  A SaaS tool that automatically generates documentation from source code, with customizable options to tailor the style and structure to the team’s needs.
+
+### Visitor Count
+
+<img src="https://profile-counter.glitch.me/Mempa/count.svg" />
+
+Last updated on Sat Sep 7 2024
+
+**X days until 2025 ⏱** days until New Year
+
+🤖 This README.md is updated with love, by Mempa ❤️
+
+<#today_date>
+
+<#day_before_new_years> days before new years
+
+<#gabot_signing>
+`;
